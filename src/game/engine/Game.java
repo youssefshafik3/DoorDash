f9966d6b6777cd971efbb697723a099e47d8ff38
dodo1222006 <minad539@gmail.com>
2026-03-06 21:08:25 +0200

    completed board and game classes

--- src/game/engine/Game.java
+++ src/game/engine/Game.java
@@ -1,5 +1,55 @@
 package game.engine;
 
-public class Game {
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class Game {
+	private Board board;
+	private	ArrayList<Monster> allMonsters;
+	private	Monster player;
+	private	Monster opponent;
+	private	Monster current;
+	
+	
+	public Game (Role playerRole) throws IOException{
+		board = new Board(DataLoader.readCards());
+		allMonsters = DataLoader.readMonsters();
+		player = selectRandomMonsterByRole(playerRole);
+		Role opponentRole =	(playerRole==SCARER)? LAUGHER:SCARER;
+		opponent = selectRandomMonsterByRole(opponentRole);
+		setCurrent(player);
+	}
+	
+	
+	
+	public Monster getCurrent() {
+		return current;
+	}
+	public void setCurrent(Monster current) {
+		this.current = current;
+	}
+	public Board getBoard() {
+		return board;
+	}
+	public ArrayList<Monster> getAllMonsters() {
+		return allMonsters;
+	}
+	public Monster getPlayer() {
+		return player;
+	}
+	public Monster getOpponent() {
+		return opponent;
+	}
+	
+	
+	
+	private Monster selectRandomMonsterByRole(Role role){
+		int sizeMonster = allMonsters.size();
+		int indexPlayer = (int)(Math.random()*sizeMonster);
+		Monster x = allMonsters.get(indexPlayer);
+		while (x.getOriginalRole()!= role){
+			indexPlayer = (int)(Math.random()*sizeMonster);
+			x = allMonsters.get(indexPlayer);
+		}
+	}
 }
