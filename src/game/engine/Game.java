ed7096bd207249c04d270c21c19cbd5744f3b62f
Youssef Shafik <youssefsameh1943@gmail.com>
2026-04-27 22:01:13 +0300

    Update the game class

--- src/game/engine/Game.java
+++ src/game/engine/Game.java
@@ -3,8 +3,10 @@ package game.engine;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 import game.engine.dataloader.DataLoader;
+import game.engine.exceptions.*;
 import game.engine.monsters.*;
 
 public class Game {
@@ -22,6 +24,11 @@ public class Game {
 		this.player = selectRandomMonsterByRole(playerRole);
 		this.opponent = selectRandomMonsterByRole(playerRole == Role.SCARER ? Role.LAUGHER : Role.SCARER);
 		this.current = player;
+		ArrayList<Monster> stationedMonsters = (ArrayList<Monster>) allMonsters.clone();
+		stationedMonsters.remove(player);
+		stationedMonsters.remove(opponent);
+		Board.setStationedMonsters(stationedMonsters);
+		board.initializeBoard(DataLoader.readCells());
 	}
 	
 	public Board getBoard() {
@@ -54,6 +61,58 @@ public class Game {
 	    		.filter(m -> m.getRole() == role)
 	    		.findFirst()
 	    		.orElse(null);
+	}
+	
+	private Monster getCurrentOpponent() {
+		return (current == player)? opponent:player;
+	}
+	
+	private int rollDice() {
+		Random r = new Random();
+		int x = r.nextInt(6)+1;
+		return x;
+	}
+	
+	public void usePowerup() throws OutOfEnergyException {
+		Monster currentOpponent = getCurrentOpponent();
+		if (current.getEnergy() >= Constants.POWERUP_COST)
+			current.executePowerupEffect(currentOpponent);
+		else
+			throw new OutOfEnergyException();
+	}
+	
+	public void playTurn() throws InvalidMoveException {
+		int roll;
+		if (current.isFrozen()) {
+			current.setFrozen(false);
+		}
+		else {
+			roll = rollDice();
+			Monster currentOpponent = getCurrentOpponent(); 
+			board.moveMonster(current, roll, currentOpponent);
+		}
+		switchTurn();
+	}
+	
+	private void switchTurn() {
+		Monster currentOpponent = getCurrentOpponent(); 
+		setCurrent(currentOpponent);
+	}
+	
+	private boolean checkWinCondition(Monster monster) {
+		int position = monster.getPosition();
+		int energy = monster.getEnergy();
+		return (position == Constants.WINNING_POSITION && energy >= Constants.WINNING_ENERGY);
+	}
+	public Monster getWinner() {
+		 if (checkWinCondition(player)) {
+			 return player;
+		 }
+		 else if (checkWinCondition(opponent)) {
+			 return opponent;
+		 }
+		 else
+			 return null;
 	}
 	
 }
