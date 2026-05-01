b8542e26abbb1d9356490b14b6e32d685c39b3e4
ahmedfathy640 <ahmedy01001@gmail.com>
2026-05-01 16:15:42 +0300

    sorry

--- src/game/engine/monsters/Schemer.java
+++ src/game/engine/monsters/Schemer.java
@@ -17,21 +17,21 @@ public class Schemer extends Monster {
         return stolen;
     }
 	@Override
-    public void setEnergy(int energy) {
-        int change = energy - this.getEnergy();
-        if (change != 0) {
-            super.setEnergy(energy + 10);
-        } else {
-            super.setEnergy(energy);
-        }
-    }
+	public void setEnergy(int energy) {
+	    super.setEnergy(energy + 10);
+	}
 	@Override
-    public void executePowerupEffect(Monster opponentMonster) {
-        int total = stealEnergyFrom(opponentMonster);
-        for (Monster m : Board.getStationedMonsters()) {
-            total += stealEnergyFrom(m);
-        }
-        this.setEnergy(this.getEnergy() + total);
-    }
+	
+	public void executePowerupEffect(Monster opponentMonster) {
+	    int total = this.stealEnergyFrom(opponentMonster);
+	    
+	    for (Monster m : Board.getStationedMonsters()) {
+	        if (m != this) { 
+	            total += stealEnergyFrom(m);
+	        }
+	    }
+	    
+	    this.setEnergy(this.getEnergy() + total);
+	}
 	
 }
